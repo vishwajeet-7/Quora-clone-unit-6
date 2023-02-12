629f2f86eb24a78889f63612d970ae3da4e82f22
Vishwajeet Singh <117368694+vishwajeet-7@users.noreply.github.com>
2023-02-12 17:26:35 +0530

    Delete my-app directory

--- my-app/src/Frontend/Api/Url.js
+++
@@ -1,3 +0,0 @@
-// export const questions = "https://kind-gold-dove-belt.cyclic.app/questions";
-export const questions = "https://new-api-bhmj.onrender.com/questions";
-// export const questions = "http://localhost:8080/questions";
--- my-app/src/Frontend/Redux/Store/store.js
+++
@@ -1,6 +0,0 @@
-import {applyMiddleware, createStore} from 'redux'
-import logger from 'redux-logger'
-import  {reducer} from '../Reducers/reducer'
-
-
- export const store = createStore(reducer,applyMiddleware(logger))
